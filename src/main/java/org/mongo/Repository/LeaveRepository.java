463c64daaa63e364520e63db7048b7f73741da50
Sahilsaxena9711 <sahilsaxena9711@gmail.com>
2018-12-14 00:14:44 +0530

    leave module added

---
+++ src/main/java/org/mongo/Repository/LeaveRepository.java
@@ -0,0 +1,17 @@
+package org.mongo.Repository;
+
+import org.mongo.Entity.Attendance;
+import org.mongo.Entity.Leave;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface LeaveRepository extends MongoRepository<Leave,Integer> {
+
+
+    List<Leave> getByApproved(boolean approved);
+
+    Leave getLeaveByUsernameAndDate(String username, String date);
+
+    List<Leave> getLeaveByUsername(String username);
+}
